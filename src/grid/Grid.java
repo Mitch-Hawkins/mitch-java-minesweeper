7302b74da791631d3d16fa1d133d508b9b6e95d8
Mitch <mitch@hawkins5.com>
2024-02-08 17:03:35 +1100

    Seperated get adjacent cell function into different mini functions for each cell, currently returns cell ascii and not the cell object itself

--- src/grid/Grid.java
+++ src/grid/Grid.java
@@ -22,13 +22,7 @@ public class Grid {
     this.rows = rows;
   }
 
-  public String createGrid() {
-    for (int i = 0; i < this.amountOfRows; i++) {
-      // rows.add(new Row(new ArrayList<Cell>(3), 3));
-      Row row = new Row(new ArrayList<Cell>(3), 3);
-      row.genRow();
-      rows.add(row);
-    }
+  public String printGrid() {
     ArrayList<String> tempString = new ArrayList<>();
     for (int i = 0; i < rows.size(); i++) {
       tempString.add(rows.get(i).printRow());
@@ -36,28 +30,91 @@ public class Grid {
     return String.join("\n", tempString);
   }
 
-  public String changeGrid() {
+  public String createGrid() {
+    for (int i = 0; i < this.amountOfRows; i++) {
+      Row row = new Row(new ArrayList<Cell>(3), 3);
+      row.genRow();
+      rows.add(row);
+    }
+    return printGrid();
+  }
+
+  public String updateGrid() {
     for (int i = 0; i < this.amountOfRows; i++) {
       for (int j = 0; j < rows.get(i).getRow().size(); j++) {
         rows.get(i).getRow().set(j, rows.get(i).getRow().get(j));
       }
     }
-    ArrayList<String> tempString = new ArrayList<>();
-    for (int i = 0; i < rows.size(); i++) {
-      tempString.add(rows.get(i).printRow());
+    return printGrid();
+  }
+
+  public String getCellToLeft(int Y, int X) {
+    if (X - 1 >= 0) {
+      return rows.get(Y).getRow().get(X - 1).getAscii();
+    } else {
+      return "N/A";
     }
-    return String.join("\n", tempString);
+  }
+
+  public String getCellToRight(int Y, int X) {
+    if (X + 1 <= rows.get(Y).getRow().size() - 1) {
+      return rows.get(Y).getRow().get(X + 1).getAscii();
+    } else {
+      return "N/A";
+    }
+  }
+
+  public String getCellAbove(int Y, int X) {
+    if (Y - 1 >= 0) {
+      return rows.get(Y - 1).getRow().get(X).getAscii();
+    } else {
+      return "N/A";
+    }
+  }
+
+  public String getCellBelow(int Y, int X) {
+    if (Y + 1 <= rows.size() - 1) {
+      return rows.get(Y + 1).getRow().get(X).getAscii();
+    } else {
+      return "N/A";
+    }
+  }
+
+  public String getAdjacentCells(int Y, int X) {
+    String currentCellAscii = rows.get(Y).getRow().get(X).getAscii();
+    String leftCellAscii = getCellToLeft(Y, X);
+    String rightCellAscii = getCellToRight(Y, X);
+    String topCellAscii = getCellAbove(Y, X);
+    String bottomCellAscii = getCellBelow(Y, X);
+    //Compile all the information
+    return String.format(
+      "Selected Cell:\n%d,%d : %s\n\nHoriz Adjacent Cells:\n%d,%d : %s\n%d,%d : %s\n\nVert Adjacent Cells:\n%d,%d : %s\n" + //
+      "%d,%d : %s",
+      Y,
+      X,
+      currentCellAscii,
+      Y,
+      (X - 1),
+      leftCellAscii,
+      Y,
+      X + 1,
+      rightCellAscii,
+      (Y - 1),
+      X,
+      topCellAscii,
+      (Y + 1),
+      X,
+      bottomCellAscii
+    );
+    //I want to print what the Cell ascii contents are
+    //I want to then find the Cell class of each horizontally adjecent Cell based on the Y and X coordinates
+    //I want to print what THOSE cell ascii contents are and label them as the adjacent cells (With Coords listed too)
   }
 }
-//    1   2   3
-// A [ ] [ ] [ ]
-// B [ ] [ ] [ ]
-// C [ ] [ ] [ ]
-// D [ ] [ ] [ ]
-// E [ ] [ ] [ ]
-//    A   B   C
-// 1 [ ] [ ] [ ]
-// 2 [ ] [ ] [ ]
-// 3 [ ] [ ] [ ]
-// 4 [ ] [ ] [ ]
-// 5 [ ] [ ] [ ]
+//Expected output of getAdjacentAscii
+//Selected Cell:
+//<CellCoords>:<CellAscii>
+//
+//Adjacent Cells:
+//<CellCoords>:<CellAscii>
+//<CellCoords>:<CellAscii>
--- src/main/Main.java
+++ src/main/Main.java
@@ -12,27 +12,26 @@ public class Main {
   public static void main(String[] args) throws Exception {
     // Row row = new Row(new ArrayList<Cell>(3), 3);
     Grid grid = new Grid(5, new ArrayList<Row>(5));
-
+    // Initialise a new grid
     System.out.println("New Grid: \n");
     System.out.println(grid.createGrid());
     System.out.println("\n");
-    // System.out.println(grid.getRows().get(0).getRow().get(0).getAscii());
+    // Set Cell 4,2 to a bomb and update grid
     grid.getRows().get(4).getRow().get(2).setAscii(new Mine().getMine());
-    // System.out.println(grid.getRows().get(0).printRow());
     System.out.println("Changed Grid: \n");
-    System.out.println(grid.changeGrid());
+    System.out.println(grid.updateGrid());
     System.out.println("\n");
-    //
+    // Set Cell 3,2 to a bomb and update grid
     grid.getRows().get(3).getRow().get(2).setAscii(new Mine().getMine());
-    //
     System.out.println("Changed Grid: \n");
-    System.out.println(grid.changeGrid());
+    System.out.println(grid.updateGrid());
     System.out.println("\n");
-    //
+    //Set Cell 3,0 to a flag and update grid
     grid.getRows().get(3).getRow().get(0).setAscii(new Flag().getFlag());
-    //
     System.out.println("Changed Grid: \n");
-    System.out.println(grid.changeGrid());
+    System.out.println(grid.updateGrid());
     System.out.println("\n");
+    //Check the contents of Cell 3,1 and the Horizontally Adjacent Cells (3,0 and 3,2)
+    System.out.println(grid.getAdjacentCells(3, 1));
   }
 }
